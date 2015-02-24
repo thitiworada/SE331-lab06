c52c50061c80eabd6d0c98c088a933017ab83e46
textFile <thitiworada_t@cmu.ac.th>
2015-02-24 23:46:05 +0700

    4.5.2.2

--- src/main/java/camt/se331/shoppingcart/config/AppConfig.java
+++ src/main/java/camt/se331/shoppingcart/config/AppConfig.java
@@ -9,6 +9,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 import org.springframework.web.servlet.view.JstlView;
 import org.springframework.web.servlet.view.UrlBasedViewResolver;
 
+@Import({PersistenceContext.class})
 @EnableWebMvc
 @Configuration 
 @ComponentScan(basePackages = {"camt.se331.shoppingcart"})
